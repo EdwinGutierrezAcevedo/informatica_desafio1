df372453a2cd022173549750ee88da172ed29cea
Aframirez14 <a.ramirez1@udea.edu.co>
2025-04-27 15:31:44 -0500

    Update FunciH.h
    Edición  y eliminación de funciones

--- Code/FunciH.h
+++ Code/FunciH.h
@@ -5,20 +5,19 @@ unsigned char* loadPixels(QString input, int &width, int &height);
 bool exportImage(unsigned char* pixelData, int width,int height, QString archivoSalida);
 unsigned int* loadSeedMasking(const char* nombreArchivo, int &seed, int &n_pixels);
 
-
-void rotarCanales(unsigned char* pixelData, int totalBytes, int rotateAmount, bool toLeft);// Operaacion Rotacion
-unsigned char rotarDerecha(unsigned char byte, int count);
-unsigned char rotarIzquierda(unsigned char byte, int count);
 bool compararDato(int dato, int datoB);
 bool verficarOperacionXor(unsigned char* arrImagen,unsigned char* arrMascara,unsigned int* arrTxt,unsigned char* arrXor, int tamMascara, int semilla );
-bool xorOperacion(unsigned char* pixelData, unsigned char* arrMascara,unsigned int* ArrTxt, unsigned char* otherData, int totalBytes,int tamMascara,int semilla);//Operacion Xor
+bool xorOperacion(unsigned char* pixelData, unsigned char* arrMascara,unsigned int* ArrTxt, unsigned char* otherData, int totalBytes,int tamMascara,int semilla, int numOrden);//Operacion Xor
 bool verificarOperacionRotacion(unsigned char* arrImagen, unsigned char* arrMascara, unsigned int* arrTxt, int tamMascara, int semilla, int numeroRot, bool izquierda);
-bool rotacionOperacion(unsigned char* pixelData, unsigned char* arrMascara, unsigned int* ArrTxt, int totalBytes, int tamMascara, int semilla, int numeroRot, bool izquierda);
+bool rotacionOperacion(unsigned char* pixelData, unsigned char* arrMascara, unsigned int* ArrTxt, int totalBytes, int tamMascara, int semilla, int numeroRot, bool izquierda, int numOrden);
 bool verficarOperacionDesplazamiento(unsigned char* arrImagen,unsigned char* arrMascara,unsigned int* arrTxt, int tamMascara, int semilla, int numeroDesp ,bool izquierda );
-bool despOperacion(unsigned char* pixelData,unsigned char* arrMascara,unsigned int* ArrTxt, int totalBytes,int tamMascara, int semilla, int numeroDesp ,bool izquierda );
-void unionOperacion(unsigned char* pixelData, unsigned char* arrMascara, unsigned int* ArrTxt, unsigned char* arrXor, int totalBytes, int tamMascara, int semilla);
+bool despOperacion(unsigned char* pixelData,unsigned char* arrMascara,unsigned int* ArrTxt, int totalBytes,int tamMascara, int semilla, int numeroDesp ,bool izquierda, int numOrden);
+
+void unionOperacion(unsigned char* pixelData, unsigned char* arrMascara, unsigned int* ArrTxt, unsigned char* arrXor, int totalBytes, int tamMascara, int semilla, int numOrden);
 void procesarArchivos(int numArchivos,unsigned char *pixelData, unsigned char *arrMascara, unsigned char *arrXor, int totalBytes);
 #endif // FUNCIONES_H
+
+
 
 
 
